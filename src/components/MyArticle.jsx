212d4161c689acce70b21514b16040958e1b5266
ckck912ck-lang <ckck912ck@gmail.com>
2026-06-24 14:46:56 +0900

    feat: Delete

--- src/components/MyArticle.jsx
+++ src/components/MyArticle.jsx
@@ -1,5 +1,5 @@
 //MyArticle 컴포넌트
-function MyArticle({ title, desc, onChangeMode }) {
+function MyArticle({ title, desc, onChangeMode, onDelete }) {
   console.log("MyArticle render");
   return (
     <section>
@@ -14,7 +14,13 @@ function MyArticle({ title, desc, onChangeMode }) {
       >
         수정
       </button>
-      <button>삭제</button>
+      <button
+        onClick={() => {
+          onDelete();
+        }}
+      >
+        삭제
+      </button>
     </section>
   );
 }
